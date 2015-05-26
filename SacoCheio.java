fd6b15f201e9fb0e96ac7f642fe9c00d09f46a40
crisdsandrade <cris.stos.andrade@gmail.com>
2015-05-26 00:36:35 -0300

    SacoCheio
    SacoCheio

---
+++ SacoCheio.java
@@ -0,0 +1,16 @@
+public class SacoCheio {
+	public static void main(String[] args) {
+		
+		int one = 103;
+		int two = 905;
+		int resultado = 2;
+
+		System.out.printf("Soma %d  ", one + two);
+			System.out.printf("Subtracao %d"   , two - one);
+				System.out.printf("Multiplicacao %d  ", one * two);
+					System.out.printf("Porcentagem %d  ", 5 / 2);
+						System.out.printf("Divisao %d  ", 10 / 2);
+		
+
+	}
+}
